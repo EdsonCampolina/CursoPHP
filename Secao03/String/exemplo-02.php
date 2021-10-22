fa278e0d95c4e3c72e2da532a131dd1c93d4e5bc
Edson Júnior Campolina Silva <69157849+EdsonCampolina@users.noreply.github.com>
2021-10-22 07:24:49 -0300

    Adicionando Secao05 + Alterando nome das seções

---
+++ Secao03/String/exemplo-02.php
@@ -0,0 +1,16 @@
+<?php  
+
+$nome = "edson junior campolina silva";
+echo strtoupper($nome);
+echo "<br/>";
+
+$nome1="EDSON JUNIOR CAMPOLINA SILVA";
+echo strtolower($nome1);
+echo "<br/>";
+
+echo ucwords($nome);
+echo "<br/>";
+
+echo ucfirst($nome);
+echo "<br/>";
+?>
---
+++ Secao03/variaveis/exemplo-01.php
@@ -0,0 +1,9 @@
+<?php 
+
+$nome = "Edson";
+
+//echo $nome;
+
+var_dump($nome);
+
+?>
